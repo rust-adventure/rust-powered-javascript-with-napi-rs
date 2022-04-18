56cb4c2d2fdb4880d6d7db82e0059dd0672ef1e0
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-18 03:30:03 -0700

    options object

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,79 @@
-import test from 'ava'
+import test from "ava";
 
-import { sum } from '../index.js'
+import { sum, compile } from "../index.js";
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test("sum from native", (t) => {
+  t.is(sum(1, 2), 3);
+});
+
+test("compile with undefined options", (t) => {
+  const markdown_input =
+    "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+  const expected_html =
+    "<p>Hello world, this is a ~~complicated~~ <em>very simple</em> example.</p>\n";
+  t.is(compile(markdown_input), expected_html);
+});
+
+test("compile with strikethrough", (t) => {
+  const markdown_input =
+    "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+  const expected_html =
+    "<p>Hello world, this is a <del>complicated</del> <em>very simple</em> example.</p>\n";
+  t.is(compile(markdown_input, { strikethrough: true }), expected_html);
+});
+
+test("compile with tables", (t) => {
+  const markdown_input = `| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |`;
+
+  const expected_html = `<table><thead><tr><th>First Header</th><th>Second Header</th></tr></thead><tbody>
+<tr><td>Content Cell</td><td>Content Cell</td></tr>
+<tr><td>Content Cell</td><td>Content Cell</td></tr>
+</tbody></table>
+`;
+  t.is(compile(markdown_input, { tables: true }), expected_html);
+});
+
+test("compile with footnotes", (t) => {
+  const markdown_input = `An example[^example] of the thing.
+
+[example]: https://example.org`;
+
+  const expected_html = `<p>An example<sup class="footnote-reference"><a href="#example">1</a></sup> of the thing.</p>\n`;
+  t.is(compile(markdown_input, { footnotes: true }), expected_html);
+});
+
+test("compile with tasklists", (t) => {
+  const markdown_input = `- [ ] one
+- [x] two
+- [ ] three`;
+
+  const expected_html = `<ul>
+<li><input disabled="" type="checkbox"/>
+one</li>
+<li><input disabled="" type="checkbox" checked=""/>
+two</li>
+<li><input disabled="" type="checkbox"/>
+three</li>
+</ul>
+`;
+  t.is(compile(markdown_input, { tasklists: true }), expected_html);
+});
+
+test("compile with smart punctuation", (t) => {
+  const markdown_input = `a quote "from someone", allegedly.`;
+
+  const expected_html = `<p>a quote “from someone”, allegedly.</p>\n`;
+  t.is(compile(markdown_input, { smartPunctuation: true }), expected_html);
+});
+
+test("compile with heading attributes", (t) => {
+  const markdown_input = `# text { #id .class1 .class2 }`;
+
+  const expected_html = `<h1 id="id" class="class1 class2">text</h1>\n`;
+  t.is(compile(markdown_input, { headingAttributes: true }), expected_html);
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,52 @@
 #![deny(clippy::all)]
 
-#[macro_use]
-extern crate napi_derive;
+// #[macro_use]
+// extern crate napi_derive;
+
+use napi_derive::napi;
+use pulldown_cmark::{html, Options, Parser};
+
+#[napi(object)]
+pub struct JSOptions {
+  pub tables: Option<bool>,
+  pub footnotes: Option<bool>,
+  pub strikethrough: Option<bool>,
+  pub tasklists: Option<bool>,
+  pub smart_punctuation: Option<bool>,
+  pub heading_attributes: Option<bool>,
+}
+
+#[napi]
+fn compile(input: String, opts: Option<JSOptions>) -> String {
+  let mut options = Options::empty();
+  if let Some(js_options) = opts {
+    if let Some(true) = js_options.tables {
+      options.insert(Options::ENABLE_TABLES);
+    }
+    if let Some(true) = js_options.footnotes {
+      options.insert(Options::ENABLE_FOOTNOTES);
+    }
+    if let Some(true) = js_options.strikethrough {
+      options.insert(Options::ENABLE_STRIKETHROUGH);
+    }
+    if let Some(true) = js_options.tasklists {
+      options.insert(Options::ENABLE_TASKLISTS);
+    }
+    if let Some(true) = js_options.smart_punctuation {
+      options.insert(Options::ENABLE_SMART_PUNCTUATION);
+    }
+    if let Some(true) = js_options.heading_attributes {
+      options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+    }
+  }
+
+  let parser = Parser::new_ext(&input, options);
+
+  // Write to String buffer.
+  let mut html_output = String::new();
+  html::push_html(&mut html_output, parser);
+  html_output
+}
 
 #[napi]
 fn sum(a: i32, b: i32) -> i32 {
